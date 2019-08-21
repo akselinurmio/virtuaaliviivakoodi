b761cc13c9e9fdd1c1c9d6c3842b8e15d2306b95
Vesa Poikajärvi <vesa.poikajarvi@iki.fi>
2019-08-21 14:16:12 +0300

    Documentation for the options

--- index.d.ts
+++ index.d.ts
@@ -7,9 +7,21 @@ declare function virtuaaliviivakoodi(opts: virtuaaliviivakoodi.Options): string;
 
 declare namespace virtuaaliviivakoodi {
   export interface Options {
+    /**
+     * Recipient IBAN
+     */
     iban: string;
+    /**
+     * Valid Finnish bank reference or ISO 11649 Creditor Reference
+     */
     reference: number;
+    /**
+     * Amount in EUR
+     */
     amount: number;
+    /**
+     * Due date in format YYMMDD
+     */
     due: string
   }
 }
