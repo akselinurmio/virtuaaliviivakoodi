3f649a261c4b39fccaac5d1e01cc245514f83b84
Akseli Nurmio <akseli@nurmio.fi>
2019-08-21 21:26:51 +0300

    Rename the main function’s parameter vars

--- index.js
+++ index.js
@@ -4,42 +4,43 @@ const methods = require('./methods')
 
 /**
  * Returns Virtuaaliviivakoodi as a string.
- * @param {Object} given - Information that will be included.
- * @param {String} given.iban - IBAN formed account number
- * @param {Number|String} given.reference - Reference number in either international or national form
- * @param {Number} [given.amount] - Amount in euros with maximum of 999999.99 and minimum of 0.01
- * @param {String} [given.due] - Due date in form of "vvkkpp" where vv is year, kk is month and pp is day
+ * @param {Object} options - Information that will be included.
+ * @param {String} options.iban - IBAN formed account number
+ * @param {Number|String} options.reference - Reference number in either international or national form
+ * @param {Number} [options.amount] - Amount in euros with maximum of 999999.99 and minimum of 0.01
+ * @param {String} [options.due] - Due date in form of "vvkkpp" where vv is year, kk is month and pp is day
  * @returns {String} Virtuaaliviivakoodi
  */
-module.exports = function Virtuaaliviivakoodi(given) {
+module.exports = function Virtuaaliviivakoodi(options) {
   // Check that "given" parameter is an object
-  if (typeof given !== 'object')
+  if (typeof options !== 'object')
     throw new Error('Object must be given as parameter')
 
-  const content = {}
+  const formatted = {}
 
   // IBAN must be given
-  if ('iban' in given) content.iban = methods.convertIBAN(given.iban)
+  if ('iban' in options) formatted.iban = methods.convertIBAN(options.iban)
   else throw new Error('No IBAN specified')
 
   // Reference must be given
-  if ('reference' in given)
-    content.reference = methods.convertReference(given.reference)
+  if ('reference' in options)
+    formatted.reference = methods.convertReference(options.reference)
   else throw new Error('No reference specified')
 
-  content.version = methods.referenceToVersion(content.reference)
+  formatted.version = methods.referenceToVersion(formatted.reference)
 
-  if ('amount' in given) content.amount = methods.convertAmount(given.amount)
-  else content.amount = methods.pad('', 8)
+  if ('amount' in options)
+    formatted.amount = methods.convertAmount(options.amount)
+  else formatted.amount = methods.pad('', 8)
 
-  if ('due' in given) content.due = methods.checkDue(given.due)
-  else content.due = methods.pad('', 6)
+  if ('due' in options) formatted.due = methods.checkDue(options.due)
+  else formatted.due = methods.pad('', 6)
 
   return (
-    content.version +
-    content.iban +
-    content.amount +
-    methods.pad(content.reference, 23) +
-    content.due
+    formatted.version +
+    formatted.iban +
+    formatted.amount +
+    methods.pad(formatted.reference, 23) +
+    formatted.due
   )
 }
