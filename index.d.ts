4eca8d9242c83c4754410a2d5e5028518142fdab
Vesa Poikajärvi <vesa.poikajarvi@iki.fi>
2019-08-22 10:33:55 +0300

    feat: add TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+// Type definitions for virtuaaliviivakoodi 1.0.3
+// Project: https://github.com/akselinurmio/virtuaaliviivakoodi
+
+export = virtuaaliviivakoodi
+
+declare function virtuaaliviivakoodi(opts: virtuaaliviivakoodi.Options): string
+
+declare namespace virtuaaliviivakoodi {
+  export interface Options {
+    /**
+     * Recipient IBAN
+     */
+    iban: string
+    /**
+     * Valid Finnish bank reference or ISO 11649 Creditor Reference
+     */
+    reference: number | string
+    /**
+     * Amount in EUR
+     */
+    amount?: number
+    /**
+     * Due date in format YYMMDD
+     */
+    due?: string
+  }
+}
