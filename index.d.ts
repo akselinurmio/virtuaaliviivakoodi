d38292ff8d0cc27331d85258cabf6e019a072caf
Akseli Nurmio <akseli@nurmio.fi>
2019-08-22 10:20:10 +0300

    Run prettier on d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,24 @@
 // Type definitions for virtuaaliviivakoodi 1.0.3
 // Project: https://github.com/akselinurmio/virtuaaliviivakoodi
 
-export = virtuaaliviivakoodi;
+export = virtuaaliviivakoodi
 
-declare function virtuaaliviivakoodi(opts: virtuaaliviivakoodi.Options): string;
+declare function virtuaaliviivakoodi(opts: virtuaaliviivakoodi.Options): string
 
 declare namespace virtuaaliviivakoodi {
   export interface Options {
     /**
      * Recipient IBAN
      */
-    iban: string;
+    iban: string
     /**
      * Valid Finnish bank reference or ISO 11649 Creditor Reference
      */
-    reference: number|string;
+    reference: number | string
     /**
      * Amount in EUR
      */
-    amount?: number;
+    amount?: number
     /**
      * Due date in format YYMMDD
      */
